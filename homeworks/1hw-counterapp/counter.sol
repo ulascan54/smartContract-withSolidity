1c6ec3c4c3205f8210f4d80a0b7d1fa56d39449d
Ulaş Can <ulascan.demirbag@std.yeditepe.edu.tr>
2023-01-10 00:23:46 +0300

    added commend line for counter app

--- homeworks/1hw-counterapp/counter.sol
+++ homeworks/1hw-counterapp/counter.sol
@@ -1,18 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.17; //this is version of code
 
 contract Counter {
-    uint256 public count;
+    //I created a contract: Counter
+    uint256 public count; // I created a variable of positive numbers
 
     function get() public view returns (uint256) {
         return count;
     }
 
     function inc() public {
+        // I created this function for increasing the count
         count += 1;
     }
 
     function dec() public {
+        // I created this function for decreasing the count
         count -= 1;
     }
 }
